5e178c0e7fd05aa5c830ce9080dd995c26525777
ZiadAlMoqamar <ziad.almoqamar@gmail.com>
2020-12-21 20:04:20 +0200

    fixing themedata

--- LibraryManagmentSystemUI/lib/theme.dart
+++ LibraryManagmentSystemUI/lib/theme.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
 
 ThemeData mainTheme = ThemeData(
+  tabBarTheme: TabBarTheme(labelColor: Colors.white),
+  primaryIconTheme: IconThemeData(color: Colors.white),
+  hintColor: Colors.grey[600],
+  primaryTextTheme: TextTheme(
+    headline6: TextStyle(color: Colors.white),
+  ),
   primaryColor: Colors.lightBlue,
   primarySwatch: Colors.lightBlue,
   accentColor: Colors.white,
@@ -9,6 +15,7 @@ ThemeData mainTheme = ThemeData(
   scaffoldBackgroundColor: Colors.grey[300],
   visualDensity: VisualDensity.adaptivePlatformDensity,
   buttonTheme: ButtonThemeData(
+    buttonColor: Colors.lightBlue,
     textTheme: ButtonTextTheme.accent,
     shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
     disabledColor: Colors.grey,
@@ -17,14 +24,14 @@ ThemeData mainTheme = ThemeData(
     bodyText1: TextStyle(fontSize: 14, color: Colors.grey[900]),
     bodyText2: TextStyle(fontSize: 16),
     headline1: TextStyle(
-        fontSize: 16, color: Colors.black, fontWeight: FontWeight.bold),
+        fontSize: 18, color: Colors.grey[600], fontWeight: FontWeight.bold),
     headline2: TextStyle(
         fontSize: 18, color: Colors.black, fontWeight: FontWeight.bold),
     headline3: TextStyle(
         fontSize: 20, color: Colors.black, fontWeight: FontWeight.bold),
     button: TextStyle(fontSize: 16, color: Colors.white),
     caption: TextStyle(fontSize: 14, color: Colors.black),
-    subtitle1: TextStyle(fontSize: 14, color: Colors.grey),
+    subtitle1: TextStyle(fontSize: 20, color: Colors.grey[900]),
     subtitle2: TextStyle(fontSize: 12, color: Colors.grey),
   ),
 );
