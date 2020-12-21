ee3685bebc0507e91b288f941b56d2ed1262a6eb
FadyAhmed99 <73797597+FadyAhmed99@users.noreply.github.com>
2020-12-22 01:07:57 +0200

    2 screen template added

---
+++ LibraryManagmentSystemUI/lib/screen/regestration/library_screen.dart
@@ -0,0 +1,22 @@
+
+import 'package:LibraryManagmentSystem/model/library.dart';
+import 'package:flutter/material.dart';
+
+class LibraryScreen extends StatefulWidget {
+  final Library library;
+  LibraryScreen({this.library});
+  @override
+  _LibraryScreenState createState() => _LibraryScreenState();
+}
+
+class _LibraryScreenState extends State<LibraryScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.library.address),
+        centerTitle: true,
+      ),
+    );
+  }
+}
